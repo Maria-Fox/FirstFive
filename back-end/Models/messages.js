2f6ea6203f631135dfba519c41bb63b1b6ee7ec8
Maria Lazaro <maria.lazaro.fox@gmail.com>
2023-01-12 11:20:24 -0800

    complete most of messages model

--- back-end/Models/messages.js
+++ back-end/Models/messages.js
@@ -7,7 +7,8 @@ class Message {
 
   // Creates new message. AUTH REQUIRED. Returns id, message_from, message_to, body, sent_at.
 
-  static async createMessage({from_username, to_username, body}) {
+  static async createMessage({message_from, message_to, body}) {
+    // passing in the SQL native current_timestamp func for msg created at.
     const newMessage = await db.query(
         `INSERT INTO messages (
               message_from,
@@ -16,26 +17,25 @@ class Message {
               sent_at)
             VALUES ($1, $2, $3, current_timestamp)
             RETURNING id, message_from, message_to, body, sent_at`,
-        [from_username, to_username, body]);
+        [message_from, message_to, body]);
 
     return newMessage.result.rows[0];
   };
 
   // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
-  // Mark given message read.. AUTH REQUIRED. Returns id, message_from, message_to, body, sent_at.
+  // View individual message by id. AUTH REQUIRED. Returns id, message_from, message_to, body, sent_at.
 
-  static async markMessageRead({id}){
+  static async viewMessageID({id}){
 
-    const result = await db.query(
-      `SELECT m.id,
-              m.message_from,
-              f.username AS from_username,
+    const messageSearchReq = await db.query(
+      `SELECT m.message_from,
+              f.username AS message_from
               f.contact_email AS from_contact_email,
               f.contact_num AS from_contact_num
               m.message_to,
-              t.username AS to_username,
-              t.contact_email AS tocontact_email,
+              t.username AS message_to,
+              t.contact_email AS to_contact_email,
               t.contact_num AS to_contact_num,
               m.body,
               m.sent_at,
@@ -46,8 +46,55 @@ class Message {
         WHERE m.id = $1`,
       [id]);
 
+      // unsure if I even need to join. Look into this...
+      // SELECT (message_from, message_to, body, sent_at) FROM messages WHERE id = 1;
+
+      let message = messageSearchReq.result.rows[0];
+      // if(!message) throw new ExpressError("Message does not exist.", 404)
+      if(!message) console.log("Should throw NotFoundError");
+
+      // returning structured object w/ destructed message content
+      return {
+        id: message.id,
+        from_user : {
+          username : message.message_from,
+          contact_email: message.from_contact_email,
+          contact_num: message.from_contact_num
+        },
+        to_user: {
+          username: message.message_to,
+          contact_email: message.to_message_email,
+          contact_num: message.to_contact_num
+        },
+        body: message.body,
+        sent_at: message.sent_at,
+        read_at: message.read_at
+      }
   }
 
+  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
+  // Mark message a read via id. AUTH REQUIRED.
 
+  static async markMessageRead({id}){
+
+    // using SQL timestamp function returns date time
+    let message = await db. query (
+      `UPDATE messages
+      SET read_at = $1
+      WHERE id = $2
+      RETURNING id, message_from, message_to, body, sent_at, read_at`,
+      [current_timestamp, id]
+    );
+
+    let updatedMessage = message.result.rows[0];
+
+    // if(!updatedMessage) throw new ExpressError("Message does not exist.", 404);
+    if(!updatedMessage) console.log("Should throw express error message id does not exist.");
+
+    return updatedMessage;
+
+    };
+
+    // closing class braket - do not delete
 }
