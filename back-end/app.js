57ac250712757e1df71ccfce28c4ad36bb8a6dd3
Maria Lazaro <maria.lazaro.fox@gmail.com>
2023-01-13 11:40:57 -0800

    add json schemas

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const authRoutes = require("./routes/userAuth");
 
 app.use(express.json());
+
+// app use this prefix for routes in this file.
+app.use("/auth", authRoutes)
 
 
 
---
+++ back-end/routes/userAuth.js
@@ -0,0 +1,7 @@
+// Includes register and login routes for user authentication.
+const User = require("../Models/user");
+const express = require("express");
+let router = new express.Router();
+
+
+module.exports = router;
