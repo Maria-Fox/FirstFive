74e57d24f7899bfa8efd3e52fef0469f7be53c24
Maria Lazaro <maria.lazaro.fox@gmail.com>
2023-01-13 09:34:46 -0800

    complete projectMember model methods

--- back-end/Models/projectMembers.js
+++ back-end/Models/projectMembers.js
@@ -4,7 +4,7 @@ class Project_Member {
 
   // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
-  // Creates project_member. AUTH REQUIRED. Updates likes set to include the liked_username.
+  // Creates project_member. AUTH REQUIRED. Updates matches set to include project_id & username.
 
   static async addMember ({project_id, username}){
 
@@ -16,6 +16,24 @@ class Project_Member {
     );
 
     return newProjMember = newMemberReq.result.rows[0];
+  };
+
+    // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+  // Deletes project_member. AUTH REQUIRED. Updates likes set to include the liked_username.
+
+  static async deleteMember ({project_id, username}){
+
+    let deleteMemberReq = await db.query(
+      `DELETE 
+      FROM prroject_members
+      WHERE project_id = $1 AND username =$2
+      RETURNING id, project_id, username`,
+      [project_id, username]
+    );
+
+    let deletionConfirmation = deleteMemberReq.result.rows[0];
+    if(!deletionConfirmation) return new ExpressError(`Porject or username do not exist. Unable to delte.`)
   };
 
 
